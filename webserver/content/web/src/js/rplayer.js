e24e2c662bf871c7a346e57c304672377fd88472
Robert Němec <engine-r@seznam.cz>
2022-02-02 22:09:27 +0100

    Fullscreen button in transport.

--- webserver/content/web/src/js/rplayer.js
+++ webserver/content/web/src/js/rplayer.js
@@ -8,16 +8,16 @@
 
 // mandatory modules
 
-import RPlayerConf from "./rplayer.conf.js?v=0924";
-import RPlayer     from "./rplayer.core.js?v=0924";
+import RPlayerConf from "./rplayer.conf.js?v=01024";
+import RPlayer     from "./rplayer.core.js?v=01024";
 
 var RPCfg = new RPlayerConf();
 var RPObj = new RPlayer(RPCfg);
 
 // optional modules
 
-import RPlayerVisual from "./rplayer.visual.js?v=0924";
-import RPlayerInfo   from "./rplayer.info.js?v=0924";
+import RPlayerVisual from "./rplayer.visual.js?v=01024";
+import RPlayerInfo   from "./rplayer.info.js?v=01024";
 
 new RPlayerVisual(RPObj);
 new RPlayerInfo(RPObj);
