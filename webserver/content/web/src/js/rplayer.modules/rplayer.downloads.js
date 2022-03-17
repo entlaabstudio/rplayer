f1d78bd5e4fc3fdc75169924311cd2af080bae97
Robert Němec <engine-r@seznam.cz>
2022-03-17 02:55:19 +0100

    Generate checkboxes and download objects for downloaded mp3 files.

--- webserver/content/web/src/js/rplayer.modules/rplayer.downloads.js
+++ webserver/content/web/src/js/rplayer.modules/rplayer.downloads.js
@@ -11,9 +11,13 @@
     constructor(
         rplayerObj
     ) {
-        this.rplayerObj     = rplayerObj;
-        this.rplayerCfg     = rplayerObj.rplayerCfg.conf;
-
+        this.rplayerObj    = rplayerObj;
+        this.rplayerCfg    = rplayerObj.rplayerCfg.conf;
+   
+        this.download      = {};
+        this.download.mp3  = [];
+        this.downloadIndex = 0;
+        
         this.init();
 
         console.log("[RPlayer]","Downloads module loaded.");
@@ -26,9 +30,44 @@
         var int = setInterval(function() {
             if (that.rplayerObj.tracklistLoaded) {
                 that.buttons();
+                that.tracks();
                 clearInterval(int);
             }
         },362);
+    }
+
+    tracks() {
+        var numOfDigits = this.numberGetDigits(Object.keys(this.rplayerCfg.album.tracks).length);
+
+        for (const [key, value] of Object.entries(this.rplayerCfg.album.tracks)) {
+            if (value.downloads.mp3) {
+                var fileName = this.numberZeroFill((parseInt(key) + 1), numOfDigits) + " - " + value.mediaName + ".mp3";
+                var checkboxId = "rplayerCheckboxDownloadMp3_" + key;
+                $("#rplayerDownloads .rplayerDownloadsTracks").append(
+                    "<div class=\"ui toggle checkbox\">" +
+                        "<input type=\"checkbox\" checked=\"checked\" id=\"" + checkboxId + "\"><label for=\"" + checkboxId + "\">" + fileName + "</label>" +
+                    "</div>"
+                );
+                this.download.mp3[key] = {
+                    mediaName: value.mediaName,
+                    fileName: fileName,
+                    download: true,
+                    srcFile: value.downloads.mp3,
+                    checkboxId: checkboxId,
+                };
+            }
+        }
+        console.log(this.download);
+    }
+
+    numberGetDigits(number) {
+        return number.toString().length;
+    }
+
+    numberZeroFill(num, size) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num;
     }
 
     buttons() {
@@ -40,7 +79,7 @@
             });
             $("#rplayerDownloads").stop().animate({
                 opacity: "1"
-            },1000);
+            },250);
         });
 
         $("#rplayerDownloads .rplayerDownloadsClose").on("click",function() {
