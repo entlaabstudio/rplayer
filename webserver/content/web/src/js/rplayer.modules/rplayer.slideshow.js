ec42923b354a80485afbe7a0e74d8289b7996a26
Robert Němec <engine-r@seznam.cz>
2022-05-06 03:59:11 +0200

    Change Lyrics timing on slideshow.

--- webserver/content/web/src/js/rplayer.modules/rplayer.slideshow.js
+++ webserver/content/web/src/js/rplayer.modules/rplayer.slideshow.js
@@ -83,7 +83,7 @@ export default class RPlayer {
                 that.getLyrics();
                 that.imageChanger();
             }
-        },17);
+        },10);
     }
 
     imageChanger() {
